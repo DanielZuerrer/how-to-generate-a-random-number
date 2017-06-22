f8b8a13e664afe03105174edcb44e74eee6a820d
zuerrdan <zuerrdan@students.zhaw.ch>
2017-06-22 14:19:32 +0200

    added title animation

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ window.addEventListener('scroll', function () {
         navigation.classList.remove('scrolling');
     }
 })
+
+var menu = [...document.querySelectorAll('a[data-scroll]')];
+var langs = menu.map(menuItem => menuItem.innerHTML);
+var langIndex = 0;
+
+var setLanguageInTitle = function () {
+    titleLang.innerHTML = langs[langIndex];
+    langIndex = (langIndex + 1) % langs.length;
+}
+
+window.setInterval(setLanguageInTitle, 50);
